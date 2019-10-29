d8fe4bfb8cefce2fbbc6e569c9576ac78319f573
Constantine <blackphoenix343@gmail.com>
2019-10-29 21:38:33 +0200

    Added extra practice material from the book

--- nlp_practice.py
+++ nlp_practice.py
@@ -44,3 +44,43 @@ def percent_word(text,word):
     count = text.count(word)
     percent = 100*count/ len(text)
     return percent
+
+#print(percent_word(chat, 'lol'))
+#list.append("item") -> append to end of list
+
+' '.join(['Monty' ,'Python']) #join list to str
+'Monty Python'.split()        #split str to list
+import matplotlib.pyplot as plt
+import pandas as pd
+from nltk.probability import FreqDist
+fdist_moby = FreqDist(moby_dick)  #frequency distribution
+fdist_bible = FreqDist(bible)
+fdist_chat = FreqDist(chat)
+
+print(fdist_moby.most_common(10))
+fdist_moby.plot(50, cumulative=True); plt.show()
+print(fdist_bible.most_common(10))
+print(fdist_chat.most_common(10))
+
+print(len(fdist_moby.hapaxes()))  #words that occur once only
+long_words = [word for word in moby_dick if len(word) >= 15] #long words 
+long_words = sorted(set([w for w in chat if len(w) > 6 and 
+              fdist_chat[w] > 7])) #long words w/ more conditions
+#length of word and numer of occurences via FreqDist
+from nltk import *
+print(bigrams(['more', 'is' , 'said', 'than', 'done']))    
+
+print('; '.join(chat.collocation_list())) #common bigrams
+
+fmoby = FreqDist([len(w) for w in moby_dick]) #freq dist of lenghts of words!
+print(fmoby.items())  #keys-values : length, apperances of words
+
+#page 44 for more functions!
+#page 45 for word comparison operators
+
+#Examples of list comprehensions 
+words_with_gnt = [w for w in moby_dick if 'gnt' in w]
+words_with_initial_capital = [w for w in Jane_austen if w.istitle()]
+words_only_digits = [w for w in chat if w.isdigit()]
+words_lower_unique = len(set([w.lower() for w in chat if w.isalpha() ])) #true vocab
+#HERE: TO SOLVE SOME CHAPTER 1 EXERCISES
